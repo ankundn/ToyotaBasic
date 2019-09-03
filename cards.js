0b6f5b7ca7c9d9fdce4b56caa4c04cdd11b028aa
ankundn <ankundanisha0@gmail.com>
2019-09-03 13:52:13 +0300

    First Commit

---
+++ cards.js
@@ -0,0 +1,64 @@
+const ValidData = () => {
+document.getElementById('the-form').addEventListener('submit', (e)=>{
+e.preventDefault();
+});
+
+    /* validation so that Customer ID isn't missing and doesnt contain any blank spaces */ 
+    const custid = document.getElementById('customer');
+    const space = new RegExp(" ");
+    if (custid.value == '' || custid.value.match(space)) {
+        alert('This field is required and should not have any blank spaces');
+        custid.style.border = '2px red solid';
+    }
+
+ /* validation so that the name field isnt left empty */
+    const name =document.getElementById('name');
+    if (name.value == ''){
+        alert('This field is required.Please input your name.');
+        name.style.border = '2px red solid';
+    }
+
+    /* validation so that the part number field isnt empty */
+  const partnum = document.getElementById('pn');
+  if (partnum.value == ''){
+      alert('This field is required. Please input your part number.');
+      partnum.style.border = '2px red solid';
+  }
+   
+/*validation so that description field isnt empty*/
+const describe = document.getElementById('desc');
+if(describe.value == ''){
+    alert('This field is required. Please put a description. ');
+    describe.style.border = '2px red solid';
+}
+
+
+  /* validation so that the price is a value greater than zero */
+const price = document.getElementById('ppp');
+if(price.value <0){
+    alert('The price should be a value greater than zero');
+    price.style.border = '2px red solid';
+}
+
+
+
+/* vaidation so that the quantity is a value greater than zero*/
+const quantity = document.getElementById('qty');
+if(quantity.value <0){
+    alert('The price should be a value greater than zero');
+    quantity.style.border = '2px red solid';
+}
+
+const UPS = document.getElementById("ups");
+    const FedExg = document.getElementById("fed");
+    const USPostA = document.getElementById("us");
+    const FedExA = document.getElementById("feda");
+    if (UPS.checked == false || FedExg.checked == false ||USPostA.checked == false || FedExA.checked == false ) {
+
+        alert('This field is required. Please select one');
+
+    }
+
+
+}
+
